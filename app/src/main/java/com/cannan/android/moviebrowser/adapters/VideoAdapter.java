d8cfcd81edfeec5332df6cfdde5d657d750471a7
Cannan <rearcobaleno@163.com>
2019-06-26 21:03:21 +0800

    feat: 修改 ViewPager、RecyclerView 联动

---
+++ app/src/main/java/com/cannan/android/moviebrowser/adapters/VideoAdapter.java
@@ -0,0 +1,10 @@
+package com.cannan.android.moviebrowser.adapters;
+
+/**
+ * @ClassName: VideoAdapter
+ * @Description:
+ * @author: Cannan
+ * @date: 2019-06-26 18:39
+ */
+public class VideoAdapter {
+}
